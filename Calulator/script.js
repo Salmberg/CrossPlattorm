0c5de87a790ef59aa864929c2593d688bf714d90
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-09-04 17:42:03 +0200

    Now it can calculate + and - and clear result

--- Calulator/script.js
+++ Calulator/script.js
@@ -1,30 +1,53 @@
-const numberElements = document.getElementsByClassName('calc_number');
+const buttonElements = document.getElementsByClassName('button');
 const resultList = document.getElementById('result_list')
+const result = document.getElementById('result')
+let firstInput = true;
 
-// for(let i=0; i< numberElements.length; i++) {
-//  numberElements[i].addEventlistener ...
-// }
 
-// Array.from(numberElements).forEach(element => {
-//     element.addEventListener 
-// })
 
-for(const numberElement of numberElements) {
-    numberElement.addEventListener('click', () => {
-        console.log('clicked' + numberElement.innerText);
+for(const buttonElement of buttonElements) {
+    buttonElement.addEventListener('click', () => {
+        buttonListener(buttonElement);
     })
 }
 
+const buttonListener = (button) => {
+    if(button.innerText == '=') {
+        console.log('Calculate button clicked');
+        calculate();
+    } else {
+        display(button.innerText);
+    }
+}
 
+const clearResult = () => {
+    result.innerText= '';
+}
 
+const display = (value) => {
+    if (firstInput) {
+        clearResult();
+        firstInput = false;
+    }
+    console.log('Displaying:', value);
+    result.innerText += value;
+}
+
+const calculate = () => {
+    let res = eval(result.innerText); // byt ut!
+    display('=' + res);
+    firstInput = true;
+}
 
 
 //Ska köras i calculate funktionen 
 // i denna --> displayResult(result.innerText);
 
-const displayResult = (value) => {
-    const listItem = document.createElement('li');
-    listItem.innerText = value;
-    resultList.appendChild(listItem)
-}
+// const displayResult = (value) => {
+//     const listItem = document.createElement('li');
+//     listItem.innerText = value;
+//     resultList.appendChild(listItem)
+// }
+
+
 
