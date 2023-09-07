75eb56bad1dc1749e986a0dc6336f5d3985de9f8
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-09-07 15:17:09 +0200

    almost done

--- Calulator/script.js
+++ Calulator/script.js
@@ -1,4 +1,5 @@
 const buttonElements = document.getElementsByClassName('button');
+const cButton = document.getElementById("cButton");
 const resultList = document.getElementById('result_list');
 const result = document.getElementById('result');
 const clearButton = document.getElementById('clearButton')
@@ -44,9 +45,19 @@ const clearButtonListener = () => {
         resultBlock.classList.add('hide');
     }
 }
-
 // Add a click event listener to the 'Clear' button.
 clearButton.addEventListener('click', clearButtonListener);
+
+
+// Define a function to clear the calculator's result.
+function clearCalculator() {
+    // Set the result display to 0 or any initial value you prefer.
+    document.getElementById("result").textContent = "0";
+    console.log('C button clicked')
+    firstInput = true;
+}
+// Attach an event listener to the clear button to call the clearCalculator function when clicked.
+cButton.addEventListener("click", clearCalculator);
 
 // Define a function to clear the result display.
 const clearResult = () => {
@@ -64,20 +75,32 @@ const display = (value) => {
     result.innerText += value;
 }
 
-// Define a function to perform the calculation.
 const calculate = () => {
-    // Evaluate the expression in the result display and store the result in 'res'.
-    let res = eval(result.innerText); // Note: Using 'eval' can be a security risk.
-    
-    // Display the result with '=' sign.
-    display('=' + res);
-    
-    // Reset the firstInput flag to true.
-    firstInput = true;
+    try {
+        // Get the expression from the result display.
+        const expression = result.innerText;
 
-    // Add the result to the result list.
-    displayResult(result.innerText);
+        // Create a new Function to evaluate the expression.
+        const calculateFunction = new Function('return ' + expression);
+
+        // Evaluate the expression and get the result.
+        const res = calculateFunction();
+
+        // Display the result with '=' sign.
+        display('=' + res);
+
+        // Reset the firstInput flag to true.
+        firstInput = true;
+
+        // Add the result to the result list.
+        displayResult(result.innerText);
+    } catch (error) {
+        // Handle any errors that occur during evaluation.
+        console.error('Error:', error);
+        // You can display an error message or take appropriate action here.
+    }
 }
+
 
 // Define a function to display the result in the result list.
 const displayResult = (value) => {
@@ -85,3 +108,6 @@ const displayResult = (value) => {
     listItem.innerText = value;
     resultList.appendChild(listItem);
 }
+
+ // Add the result to the result list.
+ //displayResult(result.innerText);
