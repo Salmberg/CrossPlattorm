3b1f23b2005162d41a7222e8315471c23e1052bb
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-09-11 10:25:49 +0200

    First setup of the Quiz with questions from API

---
+++ Grid intro/script.js
@@ -0,0 +1,10 @@
+console.log("Script loaded");
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const nav = document.querySelector("nav ul");
+
+    menuToggle.addEventListener("click", function () {
+        nav.classList.toggle("show-menu"); // Toggle the "show-menu" class
+    });
+});
